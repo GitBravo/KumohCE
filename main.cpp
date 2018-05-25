fc4fe86fb90c0d9fb27efd80956842dcbc94e29c
곽중호 <thdge8@gmail.com>
2018-05-26 00:05:30 +0900

    아스키값 1:1 대

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
+#include "note.h"
 #define PLAINTEXT_MAX_SIZE 1024 // 최대 평문 길이
-#define MELODY_LENGTH 500 // 음 길이
-#define COMMON_DIFFERENCE 100 // 공차
+#define COMMON_DIFFERENCE 50 // 공차
+
+#define MELODY_LENGTH_1 500 // 음 길이
+#define MELODY_LENGTH_2 750 // 음 길이
+#define MELODY_LENGTH_3 1000 // 음 길이
+#define MELODY_LENGTH_4 1250 // 음 길이
+#define MELODY_LENGTH_5 1500 // 음 길이
+#define MELODY_LENGTH_6 1750 // 음 길이
+#define MELODY_LENGTH_7 2000 // 음 길이
+#define MELODY_LENGTH_8 2250 // 음 길이
+#define MELODY_LENGTH_9 2500 // 음 길이
+#define MELODY_LENGTH_10 2750 // 음 길이
+#define MELODY_LENGTH_11 3000 // 음 길이
+#define MELODY_LENGTH_12 3250 // 음 길이
 using namespace std;
 
 class Melody {
@@ -13,7 +26,6 @@ public:
 	Melody(string plaintext) {
 		mStringPlaintext = plaintext;
 		mPlaintextSize = mStringPlaintext.length();
-
 		// string 타입의 문자열을 char[] 타입으로 변환
 		strncpy(mCharPlaintext, mStringPlaintext.c_str(), sizeof(mCharPlaintext));
 		mCharPlaintext[sizeof(mCharPlaintext) - 1] = 0;
@@ -28,9 +40,117 @@ public:
 	}
 
 	void Play() {
-		for (int i = 0; i < mPlaintextSize; i++) {
-			cout << "현재 음 : " << mCharPlaintext[i] << endl;
-			Beep(SeqMulti(mCharPlaintext[i]), MELODY_LENGTH);
+		for(int i = 0 ; i < mPlaintextSize ; i ++){
+			cout << "현재 아스키값 : " << mCharPlaintext[i] << endl;
+	//		Beep(SeqMulti(mCharPlaintext[i]), MELODY_LENGTH);
+			switch (mCharPlaintext[i])
+			{
+			case 33: Beep(DO1, MELODY_LENGTH_1); break;
+			case 34: Beep(DO2, MELODY_LENGTH_1); break;
+			case 35: Beep(DO3, MELODY_LENGTH_1); break;
+			case 36: Beep(DO4, MELODY_LENGTH_1); break;
+			case 37: Beep(DO5, MELODY_LENGTH_1); break;
+			case 38: Beep(DO6, MELODY_LENGTH_1); break;
+			case 39: Beep(DO7, MELODY_LENGTH_1); break;
+			case 40: Beep(DO8, MELODY_LENGTH_1); break;
+
+			case 41: Beep(RE1, MELODY_LENGTH_2); break;
+			case 42: Beep(RE2, MELODY_LENGTH_2); break;
+			case 43: Beep(RE3, MELODY_LENGTH_2); break;
+			case 44: Beep(RE4, MELODY_LENGTH_2); break;
+			case 45: Beep(RE5, MELODY_LENGTH_2); break;
+			case 46: Beep(RE6, MELODY_LENGTH_2); break;
+			case 47: Beep(RE7, MELODY_LENGTH_2); break;
+			case 48: Beep(RE8, MELODY_LENGTH_2); break;
+
+			case 49: Beep(MI1, MELODY_LENGTH_3); break;
+			case 50: Beep(MI2, MELODY_LENGTH_3); break;
+			case 51: Beep(MI3, MELODY_LENGTH_3); break;
+			case 52: Beep(MI4, MELODY_LENGTH_3); break;
+			case 53: Beep(MI5, MELODY_LENGTH_3); break;
+			case 54: Beep(MI6, MELODY_LENGTH_3); break;
+			case 55: Beep(MI7, MELODY_LENGTH_3); break;
+			case 56: Beep(MI8, MELODY_LENGTH_3); break;
+
+			case 57: Beep(PA1, MELODY_LENGTH_4); break;
+			case 58: Beep(PA2, MELODY_LENGTH_4); break;
+			case 59: Beep(PA3, MELODY_LENGTH_4); break;
+			case 60: Beep(PA4, MELODY_LENGTH_4); break;
+			case 61: Beep(PA5, MELODY_LENGTH_4); break;
+			case 62: Beep(PA6, MELODY_LENGTH_4); break;
+			case 63: Beep(PA7, MELODY_LENGTH_4); break;
+			case 64: Beep(PA8, MELODY_LENGTH_4); break;
+
+			case 65: Beep(SOL1, MELODY_LENGTH_5); break;
+			case 66: Beep(SOL2, MELODY_LENGTH_5); break;
+			case 67: Beep(SOL3, MELODY_LENGTH_5); break;
+			case 68: Beep(SOL4, MELODY_LENGTH_5); break;
+			case 69: Beep(SOL5, MELODY_LENGTH_5); break;
+			case 70: Beep(SOL6, MELODY_LENGTH_5); break;
+			case 71: Beep(SOL7, MELODY_LENGTH_5); break;
+			case 72: Beep(SOL8, MELODY_LENGTH_5); break;
+
+			case 73: Beep(RA1, MELODY_LENGTH_6); break;
+			case 74: Beep(RA2, MELODY_LENGTH_6); break;
+			case 75: Beep(RA3, MELODY_LENGTH_6); break;
+			case 76: Beep(RA4, MELODY_LENGTH_6); break;
+			case 77: Beep(RA5, MELODY_LENGTH_6); break;
+			case 78: Beep(RA6, MELODY_LENGTH_6); break;
+			case 79: Beep(RA7, MELODY_LENGTH_6); break;
+			case 80: Beep(RA8, MELODY_LENGTH_6); break;
+
+			case 81: Beep(SI1, MELODY_LENGTH_7); break;
+			case 82: Beep(SI2, MELODY_LENGTH_7); break;
+			case 83: Beep(SI3, MELODY_LENGTH_7); break;
+			case 84: Beep(SI4, MELODY_LENGTH_7); break;
+			case 85: Beep(SI5, MELODY_LENGTH_7); break;
+			case 86: Beep(SI6, MELODY_LENGTH_7); break;
+			case 87: Beep(SI7, MELODY_LENGTH_7); break;
+			case 88: Beep(SI8, MELODY_LENGTH_7); break;
+
+			case 89: Beep(HDO1, MELODY_LENGTH_8); break;
+			case 90: Beep(HDO2, MELODY_LENGTH_8); break;
+			case 91: Beep(HDO3, MELODY_LENGTH_8); break;
+			case 92: Beep(HDO4, MELODY_LENGTH_8); break;
+			case 93: Beep(HDO5, MELODY_LENGTH_8); break;
+			case 94: Beep(HDO6, MELODY_LENGTH_8); break;
+			case 95: Beep(HDO7, MELODY_LENGTH_8); break;
+			case 96: Beep(HDO8, MELODY_LENGTH_8); break;
+
+			case 97: Beep(HRE1, MELODY_LENGTH_9); break;
+			case 98: Beep(HRE2, MELODY_LENGTH_9); break;
+			case 99: Beep(HRE3, MELODY_LENGTH_9); break;
+			case 100: Beep(HRE4, MELODY_LENGTH_9); break;
+			case 101: Beep(HRE5, MELODY_LENGTH_9); break;
+			case 102: Beep(HRE6, MELODY_LENGTH_9); break;
+			case 103: Beep(HRE7, MELODY_LENGTH_9); break;
+			case 104: Beep(HRE8, MELODY_LENGTH_9); break;
+
+			case 105: Beep(HMI1, MELODY_LENGTH_10); break;
+			case 106: Beep(HMI2, MELODY_LENGTH_10); break;
+			case 107: Beep(HMI3, MELODY_LENGTH_10); break;
+			case 108: Beep(HMI4, MELODY_LENGTH_10); break;
+			case 109: Beep(HMI5, MELODY_LENGTH_10); break;
+			case 110: Beep(HMI6, MELODY_LENGTH_10); break;
+			case 111: Beep(HMI7, MELODY_LENGTH_10); break;
+			case 112: Beep(HMI8, MELODY_LENGTH_10); break;
+
+			case 113: Beep(HPA1, MELODY_LENGTH_11); break;
+			case 114: Beep(HPA2, MELODY_LENGTH_11); break;
+			case 115: Beep(HPA3, MELODY_LENGTH_11); break;
+			case 116: Beep(HPA4, MELODY_LENGTH_11); break;
+			case 117: Beep(HPA5, MELODY_LENGTH_11); break;
+			case 118: Beep(HPA6, MELODY_LENGTH_11); break;
+			case 119: Beep(HPA7, MELODY_LENGTH_11); break;
+			case 120: Beep(HPA8, MELODY_LENGTH_11); break;
+
+			case 121: Beep(HSOL1, MELODY_LENGTH_12); break;
+			case 122: Beep(HSOL2, MELODY_LENGTH_12); break;
+			case 123: Beep(HSOL3, MELODY_LENGTH_12); break;
+			case 124: Beep(HSOL4, MELODY_LENGTH_12); break;
+			case 125: Beep(HSOL5, MELODY_LENGTH_12); break;
+			case 126: Beep(HSOL6, MELODY_LENGTH_12); break;
+			}
 		}
 	}
 private:
---
+++ note.h
@@ -0,0 +1,108 @@
+#pragma once
+#define DO1 32.7032
+#define DO2 65.4064
+#define DO3 130.8128
+#define DO4 261.6256
+#define DO5 523.2511
+#define DO6 1046.502
+#define DO7 2093.005
+#define DO8 4186.009
+
+#define RE1 36.7081 
+#define RE2 73.4162
+#define RE3 146.8324
+#define RE4 293.6648
+#define RE5 587.3295
+#define RE6 1174.659
+#define RE7 2349.318
+#define RE8 4698.636
+
+#define MI1 41.2034
+#define MI2 82.4069
+#define MI3 164.8138
+#define MI4 329.6276
+#define MI5 659.2551
+#define MI6 1318.510
+#define MI7 2637.020
+#define MI8 5274.041
+
+#define PA1 43.6535
+#define PA2 87.3071
+#define PA3 174.6141
+#define PA4 349.2282
+#define PA5 698.4565
+#define PA6 1396.913
+#define PA7 2793.826
+#define PA8 5587.652
+
+#define SOL1 48.9994
+#define SOL2 97.9989
+#define SOL3 195.9977
+#define SOL4 391.9954
+#define SOL5 783.9909
+#define SOL6 1567.982
+#define SOL7 3135.963
+#define SOL8 6271.927
+
+#define RA1 55.0000
+#define RA2 110.0000
+#define RA3 220.0000
+#define RA4 440.0000
+#define RA5 880.0000
+#define RA6 1760.0000
+#define RA7 3520.0000
+#define RA8 7040.0000
+
+#define SI1 61.7354
+#define SI2 123.4708
+#define SI3 246.9417
+#define SI4 493.8833
+#define SI5 987.7666
+#define SI6 1975.533
+#define SI7 3951.066
+#define SI8 7902.133
+
+#define HDO1 65.4064
+#define HDO2 130.8128
+#define HDO3 261.6256
+#define HDO4 523.2511
+#define HDO5 1046.502
+#define HDO6 2093.005
+#define HDO7 4186.009
+#define HDO8 8372.018
+
+#define HRE1 73.4162
+#define HRE2 146.8324
+#define HRE3 293.6648
+#define HRE4 587.3295
+#define HRE5 1174.659
+#define HRE6 2349.318
+#define HRE7 4698.636
+#define HRE8 9397.272
+
+#define HMI1 82.4069
+#define HMI2 164.8138
+#define HMI3 329.6276
+#define HMI4 659.2551
+#define HMI5 1318.510
+#define HMI6 2637.020
+#define HMI7 5274.041
+#define HMI8 10548.082
+
+#define HPA1 87.3071
+#define HPA2 174.6141
+#define HPA3 349.2282
+#define HPA4 698.4565
+#define HPA5 1396.913
+#define HPA6 2793.826
+#define HPA7 5587.652
+#define HPA8 11175.304
+
+#define HSOL1 97.9989
+#define HSOL2 195.9977
+#define HSOL3 391.9954
+#define HSOL4 783.9909
+#define HSOL5 1567.982
+#define HSOL6 3135.963
+#define HSOL7 6271.927
+#define HSOL8 12543.854
